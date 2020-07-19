76ca1e3258276353cc7b3333e49908f4c0d392e4
Tom Elliott <tomelliott2405@gmail.com>
2020-07-19 08:35:59 -0400

    Initial commit

---
+++ src/test/java/com/elliott/restart/TestExample.java
@@ -0,0 +1,14 @@
+package com.elliott.restart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestExample {
+
+    @Test
+    public void testSomething() {
+       assertEquals(1, 1);
+    }
+
+}
